313ebfb9d0fa1ce61573e8119e8a009ef50bf33e
Grace Brassard-Monahan <gracemonahan4@gmail.com>
2019-12-27 17:43:03 +0100

    added gifs to the join us page

--- wp-content/themes/chaplinchild/functions.php
+++ wp-content/themes/chaplinchild/functions.php
@@ -12,7 +12,25 @@ function my_theme_enqueue_styles() {
     );
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
-function enqueue_load_fa() {
-wp_enqueue_style( 'load-fa', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+/**
+ * Font Awesome Kit Setup
+ *
+ * This will add your Font Awesome Kit to the front-end, the admin back-end,
+ * and the login screen area.
+ */
+if (! function_exists('fa_custom_setup_kit') ) {
+  function fa_custom_setup_kit($kit_url = '') {
+    foreach ( [ 'wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts' ] as $action ) {
+      add_action(
+        $action,
+        function () use ( $kit_url ) {
+          wp_enqueue_script( 'font-awesome-kit', $kit_url, [], null );
+        }
+      );
+    }
+  }
 }
+
+fa_custom_setup_kit('https://kit.fontawesome.com/1bedda7e13.js');
+
+/*end of fontawesome setup code. let's pray it works this time ! */
